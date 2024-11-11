b2a6156ab47272e9b92256b1572cce5090f41155
drsvds <dinguspodsg@gmail.com>
2024-11-11 10:30:43 +0800

    Initial Commit

---
+++ Placeholder app/Placeholder_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Placeholder_appApp.swift
+//  Placeholder app
+//
+//  Created by T Krobot on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Placeholder_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Placeholder appTests/Placeholder_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  Placeholder_appTests.swift
+//  Placeholder appTests
+//
+//  Created by T Krobot on 11/11/24.
+//
+
+import Testing
+@testable import Placeholder_app
+
+struct Placeholder_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

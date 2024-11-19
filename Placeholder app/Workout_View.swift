03b465351b42fb8728e642a3767930c8be875607
R3Gan09 <152863340+R3Gan09@users.noreply.github.com>
2024-11-19 15:50:31 +0800

    latest

--- Placeholder app/Workout_View.swift
+++ Placeholder app/Workout_View.swift
@@ -12,8 +12,8 @@ struct Workout_View: View {
     ]
     @State private var date = Date.now
     @State private var isTimerRunning = false
-    @State private var remainingTime = 1
-    @State private var workoutTime = 1
+    @State private var remainingTime = 60
+    @State private var workoutTime = 60
     @State private var showPicker: Bool = false
     @State var reps = 10
     @State var todayReps = 10
@@ -108,7 +108,7 @@ struct Workout_View: View {
         if workoutsCompleted < exercises.count - 1 {
             // Move to the next exercise
             workoutsCompleted += 1
-            remainingTime = 1 // Reset timer
+            remainingTime = 60 // Reset timer
         } else {
             // All exercises completed
             finishWorkout()
